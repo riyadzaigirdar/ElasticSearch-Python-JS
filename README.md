daa32b98aae68043ca5cebfda9cbbb0ca925f1dc
riyadzaigirdar <riyadzaigir280@gmail.com>
2020-12-16 14:01:49 +0600

    fixed track files for js

--- README.md
+++
@@ -1,5 +0,0 @@
-# ElasticSearch-Python-JS
-
-### elastic search library code for python and js
-
-### POST method is for query and adding new item
--- js/routes/client/connection.js
+++
@@ -1,8 +0,0 @@
-var elasticsearch = require("elasticsearch");
-var client = new elasticsearch.Client({
-  host: "localhost:9203",
-  log: "trace",
-  apiVersion: "7.2", // use the same version of your Elasticsearch instance
-});
-
-module.exports = client;
