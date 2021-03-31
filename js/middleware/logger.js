eb7ae7a76c810ab0c514f1a888e06a58b37dabb2
Abdus Salam Zaigirdar <42799939+riyadzaigirdar@users.noreply.github.com>
2021-03-31 18:20:33 +0600

    Update logger.js

--- js/middleware/logger.js
+++ js/middleware/logger.js
@@ -4,7 +4,7 @@ const moment = require("moment");
 
 const logger = (req, res, next) => {
   console.log(
-    `${req.method} ${req.protocol}://${req.get("host")}${
+    `${req.method} ${res.statusCode} ${req.protocol}://${req.get("host")}${
       req.originalUrl
     }: ${moment().format()}`
   );
